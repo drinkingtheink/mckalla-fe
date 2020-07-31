bdc27b258d8a7da6e505b313cdaf4b0130ba9c8d
Jason M Harrison <jason.m.harrison@gmail.com>
2020-07-30 23:44:26 -0500

    Intial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Header />
+    <MainContent />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import MainContent from './components/MainContent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    MainContent
+  }
+};
+</script>
+
+<style lang="scss">
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header 
+    id="austin-anthem-header" 
+    class="fixed-top"
+  >
+    <img 
+      src="@/assets/nav/anthem-logo-navigation.png"
+      class="anthem-logo"
+    />
+
+    <section class="anthem-nav-links">
+      <!-- nav links -->
+    </section>
+
+    <section class="anthem-lang-login">
+      <!-- language and login links -->
+    </section>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/palette';
+
+#austin-anthem-header {
+  background-color: $black;
+  color: #fff;
+  display: flex;
+  padding: .5em 1em;
+}
+
+.fixed-top {
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+}
+
+.anthem-logo {
+  max-height: 50px;
+}
+</style>
